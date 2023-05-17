d1f50a34704a3ca09c5ac46fb8471d8400c3cb86
felipefialho <hi@felipefialho.com>
2023-05-17 16:39:56 -0300

    fix: adjust build errors

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import {
   IonSelectOption,
   setupIonicReact
 } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
