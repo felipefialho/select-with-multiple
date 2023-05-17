4b933090a04b3a063dcef17085e0611a38857230
felipefialho <hi@felipefialho.com>
2023-05-17 16:34:11 -0300

    chore: add example to 'steps to reproduce'

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
+  IonSelect,
+  IonSelectOption,
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
@@ -38,40 +33,20 @@ import './theme/variables.css';
 setupIonicReact();
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+  <IonApp style={{ maxWidth: "500px", margin: "auto" }}>
+    <IonSelect
+      multiple
+      color="secondary"
+      label="Testing with multiple"
+      placeholder="Select One or More"
+      label-placement="stacked"
+      mode="md"
+    >
+      <IonSelectOption value="brown">Brown</IonSelectOption>
+      <IonSelectOption value="blonde">Blonde</IonSelectOption>
+      <IonSelectOption value="black">Black</IonSelectOption>
+    </IonSelect>
   </IonApp>
-);
+)
 
 export default App;
